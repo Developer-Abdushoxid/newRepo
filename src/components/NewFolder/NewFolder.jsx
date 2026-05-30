5de93911cfd1ef4b4b2429ec681a18d456357ad0
Shaxriyor1227 <kiyotasakiayanakoji@gmail.com>
2026-05-30 14:50:35 +0500

    NewFolder

---
+++ src/components/NewFolder/NewFolder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewFolder = () => {
+  return (
+    <div>
+        <h1>Bu meni fayilim!!</h1>
+    </div>
+  )
+}
+
+export default NewFolder
---
+++ src/components/SecondFolder/SecondFolder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SecondFolder = () => {
+  return (
+    <div>
+        <h1>hahahahah bu ham meniki</h1>
+    </div>
+  )
+}
+
+export default SecondFolder
